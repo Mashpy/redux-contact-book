5946b15ddeaada65b707b05d12309d8191bc97ac
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2021-04-16 21:54:04 +0600

    Added contact

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import React from "react";
 import './styles/App.scss';
 import Navbar from './components/elements/Navbar';
 import Contacts from './components/contacts/Contacts';
+import { Provider } from "react-redux";
+import store from "./store";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import AddContact from './components/contacts/AddContact'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <div className="container">
-        <div className="py-3">
-          <Contacts/>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar/>
+          <div className="container">
+            <div className="py-3">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+              </Switch>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddContact = () => {
+    return (
+        <div>
+            Create your contact
+        </div>
+    )
+}
+
+export default AddContact
